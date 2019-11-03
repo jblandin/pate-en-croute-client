1549eb5e21f4b870d33978116ffbcdb5981aa907
Julien Blandineau <julien.blandineau@asso-cocktail.fr>
2019-11-03 22:59:40 +0100

    Affichage de la pause automatique

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
 import { AppTimerService } from './services';
 import { HhmmssPipe } from './pipes';
 
